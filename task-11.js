aed0be12f508ae0c86b5952b4213344bdf09000e
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-05-29 19:11:31 +0300

    NODE / Движок представлений - EJS

--- task-11.js
+++ task-11.js
@@ -7,18 +7,22 @@ const port = process.env.PORT ?? 3000
 
 const __dirname = path.resolve()
 
-// Посмотрим значение константы.
-console.log(app.get('view engine'))
-
 // Установим, какое представление мы будем использовать
 app.set('view engine', 'ejs');
+
+// Посмотрим значение константы.
+console.log(app.get('view engine'))
 
 // Поменяем папку для хранения представлений
 app.set('views', path.resolve(__dirname, 'ejs'));
 
 // Рендерим необходимую страницу
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {title: 'Main Page', active: 'main'})
+})
+
+app.get('/features', (req, res) => {
+    res.render('features', {title: 'Features Page', active: 'features'})
 })
 
 app.listen(port, () => {
